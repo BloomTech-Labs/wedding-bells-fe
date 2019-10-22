be0bc994caa50ee34f52fcf7647986d1e87ed03e
schang1146 <sammychang2185@gmail.com>
2019-10-21 20:53:51 -0700

    header looks like on figma

--- weddingbells/src/App.js
+++ weddingbells/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import "./styles/App.css";
 
+import Header from "./components/Header/Header";
 import { LandingPageView } from "./views";
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				This is the best Wedding Planner App ever!
+                <Header />
                 <LandingPageView />
 			</div>
 		);
--- weddingbells/src/components/Header/Header.js
+++ weddingbells/src/components/Header/Header.js
@@ -2,14 +2,64 @@ import React, { Component } from "react";
 import styled from "styled-components";
 
 const Navbar = styled.div`
+    // border
     border: 1px red solid;
+
+    // sizing & placement
+    height: 80px;
+
+    // background
+    background-color: #c4c4c4;
+
+    // display
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+
+    // text
+    font-size: 23px;
+`;
+
+const Logo = styled.div`
+    // size & placement
+    margin-left: 75px;
+`;
+
+const Links = styled.div`
+    // size & placement
+    margin-right: 75px;
+    width: 30%;
+
+    // display
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
 `;
 
 class Header extends Component {
     render() {
         return (
-            <Navbar className="header">
-                I'm going to be a header!
+            <Navbar className="header_wrapper">
+                <Logo className="header_logo">
+                    Wedding Bells
+                </Logo>
+                <Links className="header_links">
+                    <div className="link">
+                        Services
+                    </div>
+                    <div className="link">
+                        FAQ
+                    </div>
+                    <div className="link">
+                        About Us
+                    </div>
+                    <div className="link">
+                        Login/Signup
+                    </div>
+                </Links>
             </Navbar>
         )
     }
