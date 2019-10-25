ee2f96185d914010ac8cfc8ba4be1a8c5292bf18
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-10-24 22:55:56 -0600

    Added the two files for the initial structure for the Guest and Vendor list components

---
+++ weddingbells/src/components/Guest List/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class GuestList extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			//placeholder}
+		};
+	}
+	render() {
+		return (
+			<div className="guestList">
+				<div>{/*PlaceHolder*/}</div>
+			</div>
+		);
+	}
+}
+export default GuestList;
---
+++ weddingbells/src/components/Vendor List/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class VendorsList extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			//placeholder}
+		};
+	}
+	render() {
+		return (
+			<div className="vendorsList">
+				<div>{/*PlaceHolder*/}</div>
+			</div>
+		);
+	}
+}
+
+export default VendorsList;
