81213a38e071caa6a04199f9f000d432d88e32e6
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-10-24 23:08:59 -0600

    Added Mock table to component

--- weddingbells/src/components/Vendor List/index.js
+++ weddingbells/src/components/Vendor List/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import { Table } from "reactstrap";
+
 class VendorsList extends Component {
 	constructor(props) {
 		super(props);
@@ -10,7 +12,36 @@ class VendorsList extends Component {
 	render() {
 		return (
 			<div className="vendorsList">
-				<div>{/*PlaceHolder*/}</div>
+				<Table>
+					<thead>
+						<tr>
+							<th>#</th>
+							<th>First Name</th>
+							<th>Last Name</th>
+							<th>Username</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th scope="row">1</th>
+							<td>Mark</td>
+							<td>Otto</td>
+							<td>@mdo</td>
+						</tr>
+						<tr>
+							<th scope="row">2</th>
+							<td>Jacob</td>
+							<td>Thornton</td>
+							<td>@fat</td>
+						</tr>
+						<tr>
+							<th scope="row">3</th>
+							<td>Larry</td>
+							<td>the Bird</td>
+							<td>@twitter</td>
+						</tr>
+					</tbody>
+				</Table>
 			</div>
 		);
 	}
