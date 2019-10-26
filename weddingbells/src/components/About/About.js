d79aad416b94e0c12c02b42b5caf41919b48bc1a
Poly Goveia <poly.goveia@gmail.com>
2019-10-26 11:33:33 -0700

    add team to about

--- weddingbells/src/components/About/About.js
+++ weddingbells/src/components/About/About.js
@@ -1,10 +1,52 @@
 import React from "react";
+import sunflower from "./sunflower.jpg";
 import styled from "styled-components";
 
 export default function About() {
+	const team = [
+		{
+			name: "Poly Goveia",
+			img: sunflower,
+		},
+		{
+			name: "Cedric Amaya",
+			img: sunflower,
+		},
+		{
+			name: "Robert Pedersen",
+			img: sunflower,
+		},
+		{
+			name: "Brey Batten",
+			img: sunflower,
+		},
+		{
+			name: "Sammy Chang",
+			img: sunflower,
+		},
+		{
+			name: "Dan Sample",
+			img: sunflower,
+		},
+		{
+			name: "Marco Guzman",
+			img: sunflower,
+		},
+	];
+
+	const TeamPics = styled.img`
+		height: 200px;
+		width: auto;
+	`;
+
 	return (
 		<div>
 			<h2>About Us</h2>
+			{team.map(dev => (
+				<div>
+					<TeamPics src={dev.img} />
+				</div>
+			))}
 		</div>
 	);
 }
