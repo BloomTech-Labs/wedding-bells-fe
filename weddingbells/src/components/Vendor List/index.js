c667fe16fc242ba1901143bf81fe0abd2ec475a5
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-10-26 14:13:18 -0600

    Made the text vertically aligned in the table

--- weddingbells/src/components/Vendor List/index.js
+++ weddingbells/src/components/Vendor List/index.js
@@ -13,82 +13,84 @@ class VendorsList extends Component {
 	render() {
 		return (
 			<div className="vendorsList">
-				<Button
-					className="addVendor"
-					color="primary"
-					onClick={console.log("GG, you clicked me")}
-				>
-					Add a Vendor
-				</Button>{" "}
-				<Table responsive hover bordered>
-					<thead>
-						<tr>
-							<th>
-								<h5>#</h5>
-							</th>
+				<div className="tableGroup">
+					<Button
+						className="addVendor"
+						color="primary"
+						onClick={console.log("GG, you clicked me")}
+					>
+						Add a Vendor
+					</Button>{" "}
+					<Table responsive hover bordered>
+						<thead>
+							<tr>
+								<th>
+									<h5>#</h5>
+								</th>
 
-							<th>
-								<h5>Vendor Name</h5>
-							</th>
-							<th>
-								<h5>Category</h5>
-							</th>
-							<th>
-								<h5>Update</h5>
-							</th>
-							<th>
-								<h5>Delete</h5>
-							</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<th>1</th>
-							<td>Ottos Bakery</td>
-							<td>Cakes</td>
-							<td className="editMe">
-								<a href="#">
-									<img src={require("../../assets/pencil.svg")} />
-								</a>
-							</td>
-							<td className="deleteMe">
-								<a href="#">
-									<img src={require("../../assets/delete.svg")} />
-								</a>
-							</td>
-						</tr>
-						<tr>
-							<th>2</th>
-							<td>Thorntons Flowers</td>
-							<td>Floral</td>
-							<td className="editMe">
-								<a href="#">
-									<img src={require("../../assets/pencil.svg")} />
-								</a>
-							</td>
-							<td className="deleteMe">
-								<a href="#">
-									<img src={require("../../assets/delete.svg")} />
-								</a>
-							</td>
-						</tr>
-						<tr>
-							<th>3</th>
-							<td>Jeykals and Hydes Locations</td>
-							<td>Venues</td>
-							<td className="editMe">
-								<a href="#">
-									<img src={require("../../assets/pencil.svg")} />
-								</a>
-							</td>
-							<td className="deleteMe">
-								<a href="#">
-									<img src={require("../../assets/delete.svg")} />
-								</a>
-							</td>
-						</tr>
-					</tbody>
-				</Table>
+								<th>
+									<h5>Vendor Name</h5>
+								</th>
+								<th>
+									<h5>Category</h5>
+								</th>
+								<th>
+									<h5>Update</h5>
+								</th>
+								<th>
+									<h5>Delete</h5>
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<th>1</th>
+								<td>Ottos Bakery</td>
+								<td>Cakes</td>
+								<td className="editMe">
+									<a href="#">
+										<img src={require("../../assets/pencil.svg")} />
+									</a>
+								</td>
+								<td className="deleteMe">
+									<a href="#">
+										<img src={require("../../assets/delete.svg")} />
+									</a>
+								</td>
+							</tr>
+							<tr>
+								<th>2</th>
+								<td>Thorntons Flowers</td>
+								<td>Floral</td>
+								<td className="editMe">
+									<a href="#">
+										<img src={require("../../assets/pencil.svg")} />
+									</a>
+								</td>
+								<td className="deleteMe">
+									<a href="#">
+										<img src={require("../../assets/delete.svg")} />
+									</a>
+								</td>
+							</tr>
+							<tr>
+								<th>3</th>
+								<td>Jeykals and Hydes Locations</td>
+								<td>Venues</td>
+								<td className="editMe">
+									<a href="#">
+										<img src={require("../../assets/pencil.svg")} />
+									</a>
+								</td>
+								<td className="deleteMe">
+									<a href="#">
+										<img src={require("../../assets/delete.svg")} />
+									</a>
+								</td>
+							</tr>
+						</tbody>
+					</Table>
+				</div>
 			</div>
 		);
 	}
