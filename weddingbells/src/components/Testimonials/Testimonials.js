eec8f66c6f9e955d49351d844debb56b9e5fe61f
schang1146 <sammychang2185@gmail.com>
2019-10-26 11:08:01 -0700

    progress on testimonials

---
+++ weddingbells/src/components/Testimonials/Testimonials.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+// import "../../styles/Testimonials.scss";
+
+class Testimonials extends Component {
+	render() {
+		return <div>Testimonials</div>;
+	}
+}
+
+export default Testimonials;
