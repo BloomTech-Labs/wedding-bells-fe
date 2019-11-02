f1af27a8b8be62136a9b108e1b6145e7e72379c2
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-11-02 14:50:34 -0600

    Added the vendor form folder and index.js file
    I used reactstrap to have prestyled components, and used a Select HTML5 element for a drop down so the couple can choose which vendor group the vendor belongs to. I created two helper functions handleChange and handleSubmit. The handleChange function should allow the user to select an option from a dropdown, and the handleSubmit function is still needing to be worked on, which I will do in a future commit.

---
+++ weddingbells/src/components/VendorForm/index.js
@@ -0,0 +1,83 @@
+import React from "react";
+import {
+	InputGroup,
+	InputGroupAddon,
+	InputGroupText,
+	Input,
+	Button,
+	Form,
+	FormGroup,
+	Label,
+	Input,
+	FormText,
+} from "reactstrap";
+
+class VendorForm extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: "disabled",
+		};
+	}
+
+	render() {
+		const handleChange = event => {
+			this.setState({ value: event.target.value });
+		};
+
+		const handleSubmit = event => {
+			alert("Your favorite flavor is: " + this.state.value);
+			event.preventDefault();
+		};
+		return (
+			<div className="vendorForm">
+				<Form inline className="form">
+					<FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+						<Label for="vendorName">Name Of Vendor</Label>
+						<Input
+							type="text"
+							name="vendorName"
+							id="vendorName"
+							onChange={this.handleChange}
+						/>
+					</FormGroup>
+					<FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+						<Label for="select">Select Vendor Type</Label>
+						<Input
+							value={this.state.value}
+							type="select"
+							name="select"
+							id="select"
+						>
+							<option disabled value="disabled">
+								Please choose an option
+							</option>
+							<option value="photographer">Photographer</option>
+							<option value="venue">Venue</option>
+							<option value="hairAndMakeup">Hair and Makeup</option>
+							<option value="attire">Attire</option>
+							<option value="baker">Baker</option>
+							<option value="florist">Florist</option>
+							<option value="musicPro">Music Pros</option>
+							<option value="videographer">Videographer</option>
+							<option value="stationer">Stationer</option>
+							<option value="caterer">Caterer</option>
+							<option value="officiant">Officiant</option>
+							<option value="jeweler">Jeweler</option>
+							<option value="favorsVendor">Favors Vendor</option>
+							<option value="rentalsCompany">Rentals Company</option>
+							<option value="transportationCompany">
+								Transportation Company
+							</option>
+							<option value="lightingDesigner">Lighting Designer</option>
+							<option disabled value="disabled">
+								Please choose an option
+							</option>
+						</Input>
+					</FormGroup>
+					<Button>Submit</Button>
+				</Form>
+			</div>
+		);
+	}
+}
