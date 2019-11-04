6140a0eb63fff039878c392b01eea32b4dbf54b5
robpedersendev <40696979+robpedersendev@users.noreply.github.com>
2019-11-04 16:42:45 -0700

    Added the existing dummy data tables into an array object to modify with axios

---
+++ weddingbells/src/components/Vendor List/data.js
@@ -0,0 +1,28 @@
+import editMe from "../../assets/pencil.svg";
+import deleteMe from "../../assets/delete.svg";
+
+const GUEST_DATA = [
+	{
+		id: 1,
+		vendorName: "Ottos Bakery",
+		category: "Cakes",
+		update: editMe,
+		delete: deleteMe,
+	},
+	{
+		id: 2,
+		vendorName: "Thorntons Flowers",
+		category: "Floral",
+		update: editMe,
+		delete: deleteMe,
+	},
+	{
+		id: 3,
+		vendorName: "Jeykals and Hydes Locations",
+		category: "Venues",
+		update: editMe,
+		delete: deleteMe,
+	},
+];
+
+export default GUEST_DATA;
