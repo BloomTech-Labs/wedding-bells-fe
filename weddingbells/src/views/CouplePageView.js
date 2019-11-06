324538d416ef3ddbeae09aacea98c6e014b5a042
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-11-05 22:52:21 -0700

    Added the masterGuestComponent/index.js file to the views/CouplePageView,js file. The next thing I will work on will be the routes and work in tandem with the authentication to only show these components when a user is logged in. On top of this, I will need to do the same thing for the vendor component(s)

---
+++ weddingbells/src/views/CouplePageView.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import GuestForm from "../components/masterGuestComponent/index";
+
+class CouplePageView extends Component {
+	render() {
+		return (
+			<div className="couplePageView">
+				<GuestForm />
+			</div>
+		);
+	}
+}
+
+export default CouplePageView;
