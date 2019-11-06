fc9252fbe81bb9a29265062b617e1dda85daa182
schang1146 <sammychang2185@gmail.com>
2019-11-05 19:10:10 -0800

    testing netlify's authentication

--- weddingbells/src/App.js
+++ weddingbells/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./styles/App.css";
 
 import { connect } from "react-redux";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, withRouter } from "react-router-dom";
 
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
@@ -10,27 +10,79 @@ import { LandingPageView } from "./views";
 
 import { toggleAuthModal } from "./actions";
 
+import netlifyIdentity from "netlify-identity-widget";
+
 class App extends Component {
 	render() {
+		console.log(netlifyAuth);
 		return (
 			<div className="App">
-				<Header authModalVisible={this.props.authModalVisible} toggleAuthModal={this.props.toggleAuthModal} />
+				<Header
+					authModalVisible={this.props.authModalVisible}
+					toggleAuthModal={this.props.toggleAuthModal}
+				/>
 				<Switch>
-					{/* <Route exact path="/" component={LandingPageView} /> */}
 					<Route
 						exact
 						path="/"
 						render={() => (
-							<LandingPageView authModalVisible={this.props.authModalVisible} toggleAuthModal={this.props.toggleAuthModal} />
+							<LandingPageView
+								authModalVisible={this.props.authModalVisible}
+								toggleAuthModal={this.props.toggleAuthModal}
+							/>
 						)}
 					/>
 					{/* <Route protected exact path="/SearchBar" component={SearchBar} /> */}
 				</Switch>
-				<Footer authModalVisible={this.props.authModalVisible} toggleAuthModal={this.props.toggleAuthModal} />
+				<Footer
+					authModalVisible={this.props.authModalVisible}
+					toggleAuthModal={this.props.toggleAuthModal}
+				/>
 			</div>
 		);
 	}
 }
+
+// Code that authenticates through netlify
+const netlifyAuth = {
+	isAuthenticated: false,
+	user: null,
+	authenticate(callback) {
+		this.isAuthenticated = true;
+		netlifyIdentity.open();
+		netlifyIdentity.on("login", user => {
+			this.user = user;
+			callback(user);
+		});
+	},
+	signout(callback) {
+		this.isAuthenticated = false;
+		netlifyIdentity.logout();
+		netlifyIdentity.on("logout", () => {
+			this.user = null;
+			callback();
+		});
+	},
+};
+
+// Going to replace Login/Logout in header/footer
+// Going to redirect user to logged-in view
+// const AuthButton = withRouter(({ history }) =>
+// 	netlifyAuth.isAuthenticated ? (
+// 		<p>
+// 			Welcome!{" "}
+// 			<button
+// 				onClick={() => {
+// 					netlifyAuth.signout(() => history.push("/"));
+// 				}}
+// 			>
+// 				Sign out
+// 			</button>
+// 		</p>
+// 	) : (
+// 		<p>You are not logged in.</p>
+// 	)
+// );
 
 const mapStateToProps = state => ({
 	authModalVisible: state.landingPageReducer.authModalVisible,
--- weddingbells/src/index.js
+++ weddingbells/src/index.js
@@ -10,13 +10,19 @@ import rootReducer from "./reducers";
 import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import netlifyIdentity from "netlify-identity-widget";
+
+window.netlifyIdentity = netlifyIdentity;
+// You must run this once before trying to interact with the widget
+netlifyIdentity.init();
+
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
-	<Router>
-		<Provider store={store}>
+	<Provider store={store}>
+		<Router>
 			<App />
-		</Provider>
-	</Router>,
+		</Router>
+	</Provider>,
 	document.getElementById("root")
 );
