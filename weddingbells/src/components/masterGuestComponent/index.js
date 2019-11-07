258ca5531d04e79bbd11110bf41c4e5300229c46
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-11-06 22:31:44 -0700

    Restructured the code to use pure React hooks, corrected props besing sent to the Guest data component, added comments where neccessary.

--- weddingbells/src/components/masterGuestComponent/index.js
+++ weddingbells/src/components/masterGuestComponent/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Table } from "reactstrap";
 
@@ -10,37 +10,16 @@ import OmniModal from "../Modal/index";
 import { Headers, GuestData } from "../Guest List/mappedOver";
 
 export default function GuestForm() {
-	const [state, setState] = React.useState({
-		//We just need the overall state from the database here, which will be updated by the componentDidMount/ComponentDidUpdate life cycles
-		guestsInfo: {},
-	});
+	const [guestInfo, setGuestInfo] = useState({ guestInfo: {} });
+	// const [state, setState] = React.useState({
+	// 	//We just need the overall state from the database here, which will be updated by the componentDidMount/ComponentDidUpdate life cycles
+	// 	guestsInfo: {},
+	// });
 
-	const post = "post";
+	//Since we are using React hooks, we are no longer going to use componentDidMount and on top of this, we are not going to need the usual axios.GET request as located below
 
-	const handlePost = useCallback(() => {
-		return post;
-	}, [post]);
-
-	const del = "delete";
-
-	const handleDel = useCallback(() => {
-		return del;
-	}, [del]);
-
-	useEffect(() => {
-		setState(prevState => [...prevState, "GET axios request fired"]);
-	}, [handlePost, handleDel]);
-
-	//Function for the change handler goes here
-	// function handleChange(evt) {
-	// 	const value = evt.target.value;
-	// 	setState({
-	// 		...state,
-	// 		[evt.target.name]: value,
-	// 	});
-	// }
-
-	//When the componentDidMount and the ComponentDidUpdate lifcycles occur, the GET method below will occur
+	/*
+		//When the componentDidMount and the ComponentDidUpdate lifcycles occur, the GET method below will occur
 	function getGuests() {
 		axios
 			.get("/Insert/Web/Address")
@@ -53,12 +32,24 @@ export default function GuestForm() {
 				console.error("Server Error", error);
 			});
 	}
+	*/
+
+	/* Starting from this line and down, whenever the guestInfo loads or is updated the component will re-render */
+	const fetchGuestInfo = async () => {
+		const response = await axios.get("/url/endpoint");
+		setGuestInfo(response.data);
+	};
+
+	useEffect(() => {
+		fetchGuestInfo(guestInfo);
+	}, [guestInfo]);
+	/* Ending at this line, whenever the guestInfo loads or is updated the component will re-render */
 
 	//When a user adds a guest information via the form with the modal, the following function will be what will do the action
 	function handleSubmitGuest(evt) {
 		evt.preventDefault();
 		const guest = {
-			...state,
+			...guestInfo,
 		};
 
 		axios
@@ -77,7 +68,7 @@ export default function GuestForm() {
 		evt.preventDefault();
 
 		axios
-			.delete(`/Insert/Web/Address/${state.id}`)
+			/*.delete(`/Insert/Web/Address/${state.id}`) */
 			.then(res => {
 				console.log("Deleting that guests information");
 				console.log("The guests information has been deleted");
@@ -101,11 +92,12 @@ export default function GuestForm() {
 						<Headers />
 
 						<tbody>
-							{state.guestsInfo.map((guest, idx) => (
+							{guestInfo.guestsInfo.map((guest, idx) => (
 								<GuestData
 									key={idx}
 									editMe={editMe}
 									deleteMe={deleteMe}
+									onDelete={handleDeleteGuest}
 									{...guest}
 								/>
 							))}
