3652f179d116a554813868056a6b956b22cb325a
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-11-06 22:23:16 -0700

    Change the to in the Link to an onClick

--- weddingbells/src/components/Guest List/mappedOver.js
+++ weddingbells/src/components/Guest List/mappedOver.js
@@ -2,35 +2,37 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Headers = props => {
-	return (<thead>
-		<tr>
-			<th>
-				<h5>#</h5>
-			</th>
+	return (
+		<thead>
+			<tr>
+				<th>
+					<h5>#</h5>
+				</th>
 
-			<th>
-				<h5>Name</h5>
-			</th>
-			<th>
-				<h5>Email</h5>
-			</th>
-			<th>
-				<h5>Going?</h5>
-			</th>
-			<th>
-				<h5>Responded to Invitation?</h5>
-			</th>
-			<th>
-				<h5>Plus One?</h5>
-			</th>
-			<th>
-				<h5>Update</h5>
-			</th>
-			<th>
-				<h5>Delete</h5>
-			</th>
-		</tr>
-	</thead>);
+				<th>
+					<h5>Name</h5>
+				</th>
+				<th>
+					<h5>Email</h5>
+				</th>
+				<th>
+					<h5>Going?</h5>
+				</th>
+				<th>
+					<h5>Responded to Invitation?</h5>
+				</th>
+				<th>
+					<h5>Plus One?</h5>
+				</th>
+				<th>
+					<h5>Update</h5>
+				</th>
+				<th>
+					<h5>Delete</h5>
+				</th>
+			</tr>
+		</thead>
+	);
 };
 
 export const GuestData = ({
@@ -43,24 +45,26 @@ export const GuestData = ({
 	onUpdate,
 	onDelete,
 }) => {
-	return (<React.Fragment>
-		<tr>
-			<th>{id}</th>
-			<td>{name}</td>
-			<td>{email}</td>
-			<td>{going}</td>
-			<td>{response}</td>
-			<td>{plusOne}</td>
-			<td className="editMe">
-				<Link to={onUpdate} target="_blank" rel="noopener noreferrer">
-					<img alt="edit" src={require("../../assets/pencil.svg")} />
-				</Link>
-			</td>
-			<td className="deleteMe">
-				<Link to={onDelete} target="_blank" rel="noopener noreferrer">
-					<img alt="delete" src={require("../../assets/delete.svg")} />
-				</Link>
-			</td>
-		</tr>
-	</React.Fragment>);
-}
+	return (
+		<React.Fragment>
+			<tr>
+				<th>{id}</th>
+				<td>{name}</td>
+				<td>{email}</td>
+				<td>{going}</td>
+				<td>{response}</td>
+				<td>{plusOne}</td>
+				<td className="editMe">
+					<Link onClick={onUpdate} target="_blank" rel="noopener noreferrer">
+						<img alt="edit" src={require("../../assets/pencil.svg")} />
+					</Link>
+				</td>
+				<td className="deleteMe">
+					<Link onClick={onDelete} target="_blank" rel="noopener noreferrer">
+						<img alt="delete" src={require("../../assets/delete.svg")} />
+					</Link>
+				</td>
+			</tr>
+		</React.Fragment>
+	);
+};
