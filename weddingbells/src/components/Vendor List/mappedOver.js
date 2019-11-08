4f5ef31fa28247bb3906e8ba85652f79c68305a9
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-11-07 21:25:28 -0700

    Copied the file over from Guest List folder and placed it inside the vendorlist folder

---
+++ weddingbells/src/components/Vendor List/mappedOver.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Headers = props => {
+	return (
+		<thead>
+			<tr>
+				<th>
+					<h5>#</h5>
+				</th>
+
+				<th>
+					<h5>Name</h5>
+				</th>
+				<th>
+					<h5>Email</h5>
+				</th>
+				<th>
+					<h5>Going?</h5>
+				</th>
+				<th>
+					<h5>Responded to Invitation?</h5>
+				</th>
+				<th>
+					<h5>Plus One?</h5>
+				</th>
+				<th>
+					<h5>Update</h5>
+				</th>
+				<th>
+					<h5>Delete</h5>
+				</th>
+			</tr>
+		</thead>
+	);
+};
+
+export const VendorData = ({
+	name,
+	email,
+	going,
+	response,
+	plusOne,
+	id,
+	onUpdate,
+	onDelete,
+}) => {
+	return (
+		<React.Fragment>
+			<tr>
+				<th>{id}</th>
+				<td>{name}</td>
+				<td>{email}</td>
+				<td>{going}</td>
+				<td>{response}</td>
+				<td>{plusOne}</td>
+				<td className="editMe">
+					<Link onClick={onUpdate} target="_blank" rel="noopener noreferrer">
+						<img alt="edit" src={require("../../assets/pencil.svg")} />
+					</Link>
+				</td>
+				<td className="deleteMe">
+					<Link onClick={onDelete} target="_blank" rel="noopener noreferrer">
+						<img alt="delete" src={require("../../assets/delete.svg")} />
+					</Link>
+				</td>
+			</tr>
+		</React.Fragment>
+	);
+};
