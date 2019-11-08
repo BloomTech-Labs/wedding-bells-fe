8b3694ff893e38f4549583a7c54fb466198a4ae0
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-11-07 21:21:19 -0700

    Copied over the content from the MasterGuestComponent and modified the ocntent to be relevant for the vendor information

---
+++ weddingbells/src/components/MasterVendorComponent/index.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Table } from "reactstrap";
+
+import deleteMe from "../../assets/delete.svg";
+import editMe from "../../assets/pencil.svg";
+
+import OmniModal from "../Modal/index";
+
+import { Headers, VendorData } from "../Guest List/mappedOver";
+
+export default function VendorComponent() {
+	//Use React hooks to set state
+	const [vendorInfo, setVendorInfo] = useState({ vendorInfo: {} });
+
+	/* Starting from this line and down, whenever the vendorInfo loads or is updated the component will re-render */
+	const fetchVendorInfo = async () => {
+		const response = await axios.get("/api/weddings/:weddingId/vendors");
+		setVendorInfo(response.data);
+	};
+
+	useEffect(() => {
+		fetchVendorInfo(vendorInfo);
+	}, [vendorInfo]);
+	/* Ending at this line, whenever the vendorInfo loads or is updated the component will re-render */
+
+	//When a user adds vendor information via the form with the modal, the following function will be what will do the action
+	function handleSubmitVendor(evt) {
+		evt.preventDefault();
+		const guest = {
+			...vendorInfo,
+		};
+
+		axios
+			.post("/api/weddings/:weddingId/vendors/:id", { guest })
+			.then(res => {
+				console.log("Adding that vendors information");
+				console.log("The vendors information has been added");
+			})
+			.catch(error => {
+				console.error("Server Error", error);
+			});
+	}
+	//When a user deletes vendors information via the trash icon, the following function will be what will do the action
+	function handleDeleteVendor(evt) {
+		evt.preventDefault();
+
+		axios
+			.delete("/api/weddings/:weddingId/vendors/:id")
+			.then(res => {
+				console.log("Deleting that vendors information");
+				console.log("The vendors information has been deleted");
+			})
+			.catch(error => {
+				console.error("Server Error", error);
+			});
+	}
+	return (
+		<div className="masterVendorComponent">
+			<div className="vendorList">
+				<div className="tableGroup">
+					<OmniModal
+						className="addVendor"
+						buttonLabel="Add Vendor"
+						modalTitle="Add Vendor"
+						onSubmit={handleSubmitVendor}
+					/>
+
+					<Table responsive hover bordered>
+						<Headers />
+
+						<tbody>
+							{vendorInfo.vendorInfo.map((guest, idx) => (
+								<VendorData
+									key={idx}
+									editMe={editMe}
+									deleteMe={deleteMe}
+									onDelete={handleDeleteVendor}
+									{...guest}
+								/>
+							))}
+						</tbody>
+					</Table>
+				</div>
+			</div>
+		</div>
+	);
+}
