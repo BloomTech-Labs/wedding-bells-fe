197696266fc08b3a6ce756077e104c4c89e2d82e
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-11-07 22:32:21 -0700

    Added the Vendor component into the CouplePageView Componenet

--- weddingbells/src/views/CouplePageView.js
+++ weddingbells/src/views/CouplePageView.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 
 import GuestComponent from "../components/masterGuestComponent/index";
+import VendorComponent from "../components/masterVendorComponent/index";
 
 class CouplePageView extends Component {
 	render() {
 		return (
 			<div className="couplePageView">
 				<GuestComponent />
+				<VendorComponent />
 			</div>
 		);
 	}
