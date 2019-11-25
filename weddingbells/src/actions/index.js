ee2f61c88f413f1a49830b9c06013d622941b3be
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-11-24 17:50:16 -0700

    Modified the code so it would use a .env variable instead of a hardcoded url

--- weddingbells/src/actions/index.js
+++ weddingbells/src/actions/index.js
@@ -11,24 +11,22 @@ export const SIGNUP_START = "SIGNUP_START";
 export const SIGNUP_SUCCESS = "SIGNUP_SUCCESS";
 export const TOGGLE_AUTH_MODAL = "TOGGLE_AUTH_MODAL";
 
+const BACKEND_API_URL =
+	process.env.REACT_APP_BACKEND_API_URL || `http://localhost:5000`;
+
 export const login = creds => dispatch => {
 	console.log(creds);
 	dispatch({ type: LOGIN_START });
-	return axios
-		.post("https://lambda-wedding-bells.herokuapp.com/api/auth/login", creds)
-		.then(res => {
-			localStorage.setItem("token", res.data.token);
-			localStorage.setItem("couple", JSON.stringify(res.data.couple));
-			dispatch({ type: LOGIN_SUCCESS, payload: res.data.couple });
-		});
+	return axios.post(`${BACKEND_API_URL}/api/auth/login`, creds).then(res => {
+		localStorage.setItem("token", res.data.token);
+		localStorage.setItem("couple", JSON.stringify(res.data.couple));
+		dispatch({ type: LOGIN_SUCCESS, payload: res.data.couple });
+	});
 };
 
 export const signup = creds => dispatch => {
 	dispatch({ type: SIGNUP_START });
-	return axios.post(
-		"https://lambda-wedding-bells.herokuapp.com//api/auth/register",
-		creds
-	);
+	return axios.post(`${BACKEND_API_URL}/api/auth/register`, creds);
 };
 
 export const signout = () => dispatch => {};
