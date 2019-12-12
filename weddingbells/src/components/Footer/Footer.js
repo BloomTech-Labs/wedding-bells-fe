1641a7d7f1681d87fd17244647d3539ee2dce410
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-12-11 22:51:14 -0500

    changed header to show H3rra after logged in and removed links from footer

--- weddingbells/src/components/Footer/Footer.js
+++ weddingbells/src/components/Footer/Footer.js
@@ -13,34 +13,6 @@ class Footer extends Component {
 				<Link to="/" className="footer-logo">
 					<h3>H3rra</h3>
 				</Link>
-				<div className="footer-links">
-					<HashLink to="#services" className="link">
-						Services
-					</HashLink>
-					<HashLink to="#faq" className="link">
-						FAQ
-					</HashLink>
-					<HashLink to="#about-us" className="link">
-						About Us
-					</HashLink>
-					<Link to="#" className="link" onClick={this.props.toggleAuthModal}>
-						Login/Signup
-					</Link>
-				</div>
-				{/* Why is this here?
-				<div>
-					Icons made by{" "}
-					<a
-						href="https://www.flaticon.com/authors/kiranshastry"
-						title="Kiranshastry"
-					>
-						Kiranshastry
-					</a>{" "}
-					from{" "}
-					<a href="https://www.flaticon.com/" title="Flaticon">
-						www.flaticon.com
-					</a>
-				</div> */}
 			</div>
 		);
 	}
--- weddingbells/src/components/Header/Header.js
+++ weddingbells/src/components/Header/Header.js
@@ -1,23 +1,22 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 import { HashLink } from "react-router-hash-link";
 
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import { bindActionCreators } from 'redux';
+import { bindActionCreators } from "redux";
 import { logOutUser } from "../../actions";
 import { withRouter } from "react-router-dom";
 import { Link } from "react-router-dom";
-
 
 // import styling
 import "./Header.scss";
 import ActionButton from "antd/lib/modal/ActionButton";
 
- const logoutCredentials = {
+const logoutCredentials = {
 	isLoggingIn: true,
 	isSigningUp: true,
- 	error: "",
- };
+	error: "",
+};
 
 class Header extends Component {
 	constructor(props) {
@@ -29,75 +28,72 @@ class Header extends Component {
 
 	logout = e => {
 		e.preventDefault();
-		this.props
-			.logOutUser(this.state.logoutCredentials)
-			this.props.history.push("/");
+		this.props.logOutUser(this.state.logoutCredentials);
+		this.props.history.push("/");
 	};
 
-
-
 	render() {
-		const  isAuthenticated = localStorage.couple && localStorage.token;
+		const isAuthenticated = localStorage.couple && localStorage.token;
 		if (isAuthenticated) {
 			return (
 				<div className="header-wrapper">
-					<div className="header-logo">Wedding Bells</div>
+					<Link to="/" className="header-logo">
+						<h3>H3rra</h3>
+					</Link>
 					<div className="header-links">
 						<HashLink to="#services" className="link">
 							Services
-					</HashLink>
+						</HashLink>
 						<HashLink to="#faq" className="link">
 							FAQ
-					</HashLink>
+						</HashLink>
 						<HashLink to="#about-us" className="link">
 							About Us
-					</HashLink>
+						</HashLink>
 						<HashLink to="/" className="link" onClick={this.logout}>
 							LogOut
-					</HashLink>
+						</HashLink>
 					</div>
 				</div>
 			);
 		} else {
 			return (
-			<div className="header-wrapper">
-				<Link to="/" className="header-logo">
-					<h3>H3rra</h3>
-				</Link>
-				<div className="header-links">
-					<HashLink to="#services" className="link">
-						Services
-					</HashLink>
-					<HashLink to="#faq" className="link">
-						FAQ
-					</HashLink>
-					<HashLink to="#about-us" className="link">
-						About Us
-					</HashLink>
-					<HashLink
-						to="#"
-						className="link"
-						onClick={this.props.toggleAuthModal}
-					>
-						Login/SignUp
-					</HashLink>
-				</div>
+				<div className="header-wrapper">
+					<Link to="/" className="header-logo">
+						<h3>H3rra</h3>
+					</Link>
+					<div className="header-links">
+						<HashLink to="#services" className="link">
+							Services
+						</HashLink>
+						<HashLink to="#faq" className="link">
+							FAQ
+						</HashLink>
+						<HashLink to="#about-us" className="link">
+							About Us
+						</HashLink>
+						<HashLink
+							to="#"
+							className="link"
+							onClick={this.props.toggleAuthModal}
+						>
+							Login/SignUp
+						</HashLink>
+					</div>
 				</div>
 			);
-
 		}
 	}
 }
 
 Header.propTypes = {
 	isAuthenticated: PropTypes.object.isRequired,
-	logout: PropTypes.func.isRequired
- }
+	logout: PropTypes.func.isRequired,
+};
 
 function mapStateToProps(state) {
 	return { isAuthenticated: state.session };
 }
 
-
 // export default connect(null, {logout})(withRouter(Header));
-export default connect(mapStateToProps, {logOutUser})(withRouter(Header));
+export default connect(mapStateToProps, { logOutUser })(withRouter(Header));
