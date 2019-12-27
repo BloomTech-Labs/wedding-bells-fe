f7d88e0241bae0ddb884f4a0fcd7bb727251b1f0
Dan Sample <danssample@gmail.com>
2019-12-26 22:10:02 -0800

    added an about page, edited pic files converted them to png, work in progress

--- weddingbells/src/components/About/About.js
+++ weddingbells/src/components/About/About.js
@@ -1,61 +1,63 @@
-import React from "react";
+import React, { Component } from "react";
 
 // import styling
 import "./About.scss";
 
 // import profile pictures
-import poly from "./images/poly.jpg";
-import cedric from "./images/cedric.jpg";
-import sammy from "./images/sammy.jpg";
-import marco from "./images/marco.jpg";
-import dan from "./images/dan.jpg";
-import brey from "./images/brey.jpg";
-import bob from "./images/bob.jpg";
+import poly from "./images/poly.png";
+import cedric from "./images/cedric.png";
+import sammy from "./images/sammy.png";
+import marco from "./images/marco.png";
+import dan from "./images/dan.png";
+import brey from "./images/brey.png";
+import bob from "./images/bob.png";
 
-export default function About() {
-	const team = [
-		{
-			name: "Poly Goveia",
-			img: poly,
-		},
-		{
-			name: "Cedric Amaya",
-			img: cedric,
-		},
-		{
-			name: "Robert Pedersen",
-			img: bob,
-		},
-		{
-			name: "Brey Batten",
-			img: brey,
-		},
-		{
-			name: "Sammy Chang",
-			img: sammy,
-		},
-		{
-			name: "Dan Sample",
-			img: dan,
-		},
-		{
-			name: "Marco Guzman",
-			img: marco,
-		},
-	];
+class About extends Component {
+	render() {
+		const team = [
+			{
+				name: "Poly Goveia",
+				img: poly,
+			},
+			{
+				name: "Cedric Amaya",
+				img: cedric,
+			},
+			{
+				name: "Robert Pedersen",
+				img: bob,
+			},
+			{
+				name: "Brey Batten",
+				img: brey,
+			},
+			{
+				name: "Sammy Chang",
+				img: sammy,
+			},
+			{
+				name: "Dan Sample",
+				img: dan,
+			},
+			{
+				name: "Marco Guzman",
+				img: marco,
+			},
+		];
 
-	return (
-		<div id="about-us">
-			<h2 className="about">About Us</h2>
-			<div className="team">
-				{team.map((dev, idx) => (
-					<div key={idx}>
-						<img className="team-pics" src={dev.img} alt={dev.name} />
-
-						<p>{dev.name}</p>
-					</div>
-				))}
+		return (
+			<div id="about-us">
+				<div className="team">
+					{team.map((dev, idx) => (
+						<div class="team-member" key={idx}>
+							<img className="team-pics" src={dev.img} alt={dev.name} />
+							<p>{dev.name}</p>
+						</div>
+					))}
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
+
+export default About;
--- weddingbells/src/views/index.js
+++ weddingbells/src/views/index.js
@@ -2,6 +2,7 @@
 // export { default as ${view_name} } from ${view_location};
 
 export { default as LandingPageView } from "./LandingPageView/LandingPageView";
+export { default as AboutPageView } from "./AboutPageView/AboutPageView";
 export { default as CouplePageView } from "./CouplePageView/CouplePageView";
 export { default as ProtectedView } from "./ProtectedView";
 export { default as WeddingCreationView } from "./WeddingCreationView/WeddingCreationView";
