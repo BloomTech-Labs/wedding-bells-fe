89ac65ef5b0207f270dae42ad7362bb9a9a46522
Dan Sample <danssample@gmail.com>
2019-12-30 20:58:47 -0800

    added font awesome stylesheet link in index.html, added bios and social links
    Co-Authored-By: BreyBatten <bbat104@gmail.com>

--- weddingbells/src/components/About/About.js
+++ weddingbells/src/components/About/About.js
@@ -18,30 +18,129 @@ class About extends Component {
 			{
 				name: "Poly Goveia",
 				img: poly,
+				quote:
+					"I'm a Computer Science major and software developer with a great passion for solving problems with the use of code. I love working day-to-day on projects that require multidisciplinary skill sets and solving complex issues that make the lives of others easier.",
+				github: (
+					<a target="_blank" href="https://github.com/Poly-G">
+						<i class="fa fa-github-square"></i>
+					</a>
+				),
+				linkedin: (
+					<a target="_blank" href="https://www.linkedin.com/in/poly-goveia/">
+						<i class="fa fa-linkedin-square"></i>
+					</a>
+				),
 			},
 			{
 				name: "Cedric Amaya",
 				img: cedric,
+				quote:
+					"I'm a Computer Science major and software developer with a great passion for solving problems with the use of code. I love working day-to-day on projects that require multidisciplinary skill sets and solving complex issues that make the lives of others easier.",
+				github: (
+					<a target="_blank" href="https://github.com/cedricium">
+						<i class="fa fa-github-square"></i>
+					</a>
+				),
+				linkedin: (
+					<a target="_blank" href="https://www.linkedin.com/in/cedricamaya/">
+						<i class="fa fa-linkedin-square"></i>
+					</a>
+				),
 			},
 			{
 				name: "Robert Pedersen",
 				img: bob,
+				quote:
+					"I am a Lambda School student that attends part-time in the evenings. My favorite hobbies include backpacking, camping, and fishing. If I am not working or doing anything of the above, you can find me spending time with my family.",
+				github: (
+					<a target="_blank" href="https://github.com/robpedersendev">
+						<i class="fa fa-github-square"></i>
+					</a>
+				),
+				linkedin: (
+					<a target="_blank" href="https://www.linkedin.com/in/robpedersendev">
+						<i class="fa fa-linkedin-square"></i>
+					</a>
+				),
 			},
 			{
 				name: "Brey Batten",
 				img: brey,
+				quote:
+					"I am a full-stack software developer and a student at Lambda School. I prefer backend development and working on a team. I enjoy being physically active, and pushing myself to be 1% better today than I was yesterday.",
+				github: (
+					<a target="_blank" href="https://github.com/BreyBatten">
+						<i class="fa fa-github-square"></i>
+					</a>
+				),
+				linkedin: (
+					<a
+						target="_blank"
+						href="https://www.linkedin.com/in/brey-batten-954661181/"
+					>
+						<i class="fa fa-linkedin-square"></i>
+					</a>
+				),
 			},
 			{
 				name: "Sammy Chang",
 				img: sammy,
+				quote:
+					"I'm a Computer Science major and software developer with a great passion for solving problems with the use of code. I love working day-to-day on projects that require multidisciplinary skill sets and solving complex issues that make the lives of others easier.",
+				github: (
+					<a target="_blank" href="https://github.com/schang1146">
+						<i class="fa fa-github-square"></i>
+					</a>
+				),
+				linkedin: (
+					<a
+						target="_blank"
+						href="https://www.linkedin.com/in/sammy-chang-927976a3/"
+					>
+						<i class="fa fa-linkedin-square"></i>
+					</a>
+				),
 			},
 			{
 				name: "Dan Sample",
 				img: dan,
+				quote:
+					"I am a motivated Full-stack developer with a wide range of skills in web development. Well-versed in structuring, developing and implementing interactive websites and SQL databases. I love solving everyday problems with the power of technology and enjoy the art of creating.",
+				github: (
+					<a target="_blank" href="https://github.com/DanSample">
+						<i class="fa fa-github-square"></i>
+					</a>
+				),
+				linkedin: (
+					<a
+						target="_blank"
+						href="https://www.linkedin.com/in/dan-sample-89897b183/"
+					>
+						<i class="fa fa-linkedin-square"></i>
+					</a>
+				),
+				// twitter:(
+
+				// ),
 			},
 			{
 				name: "Marco Guzman",
 				img: marco,
+				quote:
+					"I'm a Computer Science major and software developer with a great passion for solving problems with the use of code. I love working day-to-day on projects that require multidisciplinary skill sets and solving complex issues that make the lives of others easier.",
+				github: (
+					<a target="_blank" href="https://github.com/mag16">
+						<i class="fa fa-github-square"></i>
+					</a>
+				),
+				linkedin: (
+					<a
+						target="_blank"
+						href="https://www.linkedin.com/in/marco-a-guzman-44834b57/"
+					>
+						<i class="fa fa-linkedin-square"></i>
+					</a>
+				),
 			},
 		];
 
@@ -49,9 +148,16 @@ class About extends Component {
 			<div id="about-us">
 				<div className="team">
 					{team.map((dev, idx) => (
-						<div class="team-member" key={idx}>
-							<img className="team-pics" src={dev.img} alt={dev.name} />
-							<p>{dev.name}</p>
+						<div className="member-wrapper">
+							<div className="team-member" key={idx}>
+								<img className="team-pics" src={dev.img} alt={dev.name} />
+								<div className="socials">
+									<h3>{dev.github}</h3>
+									<h3>{dev.linkedin}</h3>
+								</div>
+								<p>{dev.name}</p>
+							</div>
+							<p className="quote">{dev.quote}</p>
 						</div>
 					))}
 				</div>
